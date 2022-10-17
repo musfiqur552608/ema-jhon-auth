c3e58d638537726501a253032ea8e7068b71da66
musfiqur552608 <mushfiqur15-10872@diu.edu.bd>
2022-10-18 02:12:53 +0600

    Login form making

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBddy67tJpCNSTcaqQnnAocPsRBuDh2CuU",
+  authDomain: "ema-jhon-simple-86e68.firebaseapp.com",
+  projectId: "ema-jhon-simple-86e68",
+  storageBucket: "ema-jhon-simple-86e68.appspot.com",
+  messagingSenderId: "400268915651",
+  appId: "1:400268915651:web:c43191fbf52ddd892b4376"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
