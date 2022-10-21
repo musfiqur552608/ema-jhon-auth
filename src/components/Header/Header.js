57f08ae5225833e0fa50567ae9c2a76076ff1f1b
Musfiq <mushfiqur15-10872@diu.edu.bd>
2022-10-21 20:40:03 +0600

    auth done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/UserContext';
 import logo from '../../images/Logo.svg';
 import './Header.css';
 
 const Header = () => {
+    const {user, logOut} = useContext(AuthContext)
     return (
         <nav className='header'>
             <img src={logo} alt="" />
@@ -12,8 +14,14 @@ const Header = () => {
                 <Link to="/orders">Orders</Link>
                 <Link to="/inventory">Inventory</Link>
                 <Link to="/about">About</Link>
-                <Link to="/login">Login</Link>
-                <Link to="/signup">Sign up</Link>
+                {
+                    user?.uid ?
+                        <button className='btn-logout' onClick={logOut}>Sign Out</button>
+                    :
+                    <>
+                        <Link to="/login">Login</Link>
+                        <Link to="/signup">Sign up</Link>
+                    </>}
             </div>
         </nav>
     );
